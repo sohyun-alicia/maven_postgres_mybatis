0fafe7eb7099e197c332d1e22fc2e4b398e993bf
Sohyun Wi <sohyun.wi@gmail.com>
2024-02-06 15:44:55 +0900

    Initial commit

---
+++ src/main/java/com/example/maven_postgres_mybatis/MavenPostgresMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven_postgres_mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenPostgresMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenPostgresMybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maven_postgres_mybatis/MavenPostgresMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven_postgres_mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPostgresMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
